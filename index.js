4044a653b5edad3c38984530f1ad88b7005f3f76
Ranielly Ferreira <eu@raniellyferreira.com.br>
2020-05-14 16:48:55 -0300

    improve code

--- index.js
+++ index.js
@@ -3,45 +3,49 @@ const axios = require('axios');
 
 const isValid = cep => /^[\d]{5}-?[\d]{3}$/.test(cep);
 
-const strip = cep => cep.replace(/[^\d]/g,'');
+const strip = cep => cep.replace('-','');
+
+const client = axios.create({
+    baseURL: `https://cep.awesomeapi.com.br/json`,
+    method: 'GET',
+    headers: {
+        'User-Agent': 'axios; awesome-cep.npm',
+        'User-Key': '3fc2702b68b95b00e61ceacdea4536eac3c911b4'
+    },
+});
 
 const findCEP = async (cep) => {
+
+    if (!cep) {
+        throw { message: 'CEP inválido', code: 400, errorCode: 'invalid' };
+    }
+
+    if(!isValid(cep)) {
+        throw { message: 'CEP inválido', code: 400, errorCode: 'invalid' };
+    }
+
     const cepNumber = strip(cep);
 
-    return new Promise((resolve, reject) => {
-
-        if (!cepNumber) {
-            return reject({message: 'CEP inválido', code: 400, errorCode: 'invalid'});
-        }
-    
-        if(!isValid(cepNumber)) {
-            return reject({message: 'CEP inválido', code: 400, errorCode: 'invalid'});
-        }
-
-        axios({
-            url: `https://cep.awesomeapi.com.br/json/${cepNumber}?r=npm`,
-            method: 'GET',
-            headers: {
-                'User-Agent': 'Request-promise; awesome-cep.npm',
-                'User-Key': '3fc2702b68b95b00e61ceacdea4536eac3c911b4'
-            }
-        }).then(({ data }) => {
-            resolve(data);
-        }).catch((err) => {
-            const  { response: { status, data } } = err;
-
-            switch(status) {
-                case 404:
-                    return reject({ message: `CEP não encontrado`, code: 404, errorCode: 'not_found' });
-                
-                case 400:
-                    return reject({ message: `CEP inválido`, code: 400, errorCode: 'invalid' });
-    
-                default:
-                    return reject(err);
-            }
+    try {
+        const { data } = await client({
+            url: `/${cepNumber}?r=npm`,
         });
-    })
+
+        return data;
+    } catch (err) {
+        const  { response: { status } } = err;
+
+        switch(status) {
+            case 404:
+                throw { message: `CEP não encontrado`, code: 404, errorCode: 'not_found' };
+
+            case 400:
+                throw { message: `CEP inválido`, code: 400, errorCode: 'invalid' };
+
+            default:
+                throw { message: err.message || '', code: 500, errorCode: 'unknown' };
+        }
+    }
 }
 
 module.exports = {
